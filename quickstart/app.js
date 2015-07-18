b35b5d795413d9886cb1b25f30ac7d4e1d2af0f5
Tesser Paolo <p.tesser921@gmail.com>
2015-07-18 16:01:27 +0200

    [quickstart] chore: complete first part of the guide END

--- quickstart/app.js
+++ quickstart/app.js
@@ -1,3 +1,30 @@
+// import the type definitions from Angular
 /// <reference path="typings/angular2/angular2.d.ts" />
-3;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
+    switch (arguments.length) {
+        case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
+        case 3: return decorators.reduceRight(function(o, d) { return (d && d(target, key)), void 0; }, void 0);
+        case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
+    }
+};
+define(["require", "exports", 'angular2/angular2'], function (require, exports, angular2_1) {
+    // Annotation section
+    var MyAppComponent = (function () {
+        function MyAppComponent() {
+            this.name = 'Paolo';
+        }
+        MyAppComponent = __decorate([
+            angular2_1.Component({
+                selector: 'my-app'
+            }),
+            angular2_1.View({
+                template: '<h1>Hello {{ name }}</h1>'
+            })
+        ], MyAppComponent);
+        return MyAppComponent;
+    })();
+    // to load your new component into its page
+    angular2_1.bootstrap(MyAppComponent);
+});
 //# sourceMappingURL=app.js.map
--- quickstart/app.ts
+++ quickstart/app.ts
@@ -1,2 +1,29 @@
+// import the type definitions from Angular
 /// <reference path="typings/angular2/angular2.d.ts" />
-3
+
+// above import statement uses ES6 module syntax to import three symbols from the Angular module
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+// Annotation section
+@Component({
+    selector: 'my-app'
+})
+@View({
+    template: '<h1>Hello {{ name }}</h1>'
+
+})
+
+// Component controller
+class MyAppComponent{
+
+    public name: string;
+
+    constructor(){
+        this.name = 'Paolo';
+    }
+
+}
+
+
+// to load your new component into its page
+bootstrap(MyAppComponent);
