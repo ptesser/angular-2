9190f95e225a25bcfd47a44ccd4a563726ef834f
Tesser Paolo <p.tesser921@gmail.com>
2015-12-10 21:03:07 +0100

    chore (all): remove old tutorial outdated

--- webpack-starter/src/app/services/services.ts
+++
@@ -1,8 +0,0 @@
-/// <reference path="../../typings/_custom.d.ts" />
-
-import {todoInjectables} from './TodoService';
-
-// Include injectables that you want to have globally throughout our app
-export var appServicesInjectables: Array<any> = [
-  todoInjectables
-];
