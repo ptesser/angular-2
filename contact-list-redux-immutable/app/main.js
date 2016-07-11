29230d243e8258ae26ea6a2efa4b621c7cbee2ad
ptesser <p.tesser921@gmail.com>
2016-07-11 23:31:34 +0200

    refactor (*js): remove some files

--- contact-list-redux-immutable/app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var contact_list_component_1 = require('./contact-list-component/contact-list.component');
-var contact_store_1 = require('./contact-store');
-platform_browser_dynamic_1.bootstrap(contact_list_component_1.ContactListComponent, [contact_store_1.ContactStore]);
-//# sourceMappingURL=main.js.map
