a3197c8bffa0f77a9c921b459100a2b0fdd875ce
ptesser <p.tesser921@gmail.com>
2016-09-03 17:56:40 +0200

    [simple-reddit] chore (app): start with hello word example

--- simple-reddit-clone/app.ts
+++ simple-reddit-clone/app.ts
@@ -1,1 +1,34 @@
 // your code goes here
+
+/**
+* A basic hello-world Angular 2 app
+*/
+
+import {
+    NgModule,
+    Component
+} from '@angular/core';
+
+import { BrowserModule }            from '@angular/platform-browser';
+import { platformBrowserDynamic }   from '@angular/platform-browser-dynamic';
+
+@Component({
+    selector: 'hello-world',
+    template: `
+    <div>
+        Hello World Paolo
+    </div>
+    `
+})
+
+class HelloWordComponent{ }
+
+@NgModule({
+    declarations:   [ HelloWordComponent ],
+    imports:        [ BrowserModule ],
+    bootstrap:      [ HelloWordComponent ],
+})
+
+class HelloWordAppModule{ }
+
+platformBrowserDynamic().bootstrapModule(HelloWordAppModule);
