672cc5b758e28eb480ad23ffe94d81ba1a5a32ce
ptesser <p.tesser921@gmail.com>
2016-09-13 09:31:13 +0200

    [sku] style (app): indent

--- sku-forms/app/ts/app.ts
+++ sku-forms/app/ts/app.ts
@@ -4,9 +4,9 @@
 import {
   Component
 } from '@angular/core';
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { NgModule }               from '@angular/core';
+import { BrowserModule }          from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import {
   FormsModule,
   ReactiveFormsModule
