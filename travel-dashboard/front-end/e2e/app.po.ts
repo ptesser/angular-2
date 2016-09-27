c819479e8b105f2babb3e9be01959d04861f45b0
ptesser <p.tesser921@gmail.com>
2016-09-28 00:23:04 +0200

    [travel] refactor (all): remove to put in a single repository

--- travel-dashboard/front-end/e2e/app.po.ts
+++
@@ -1,9 +0,0 @@
-export class TravelDashboardPage {
-  navigateTo() {
-    return browser.get('/');
-  }
-
-  getParagraphText() {
-    return element(by.css('travel-dashboard-app h1')).getText();
-  }
-}
--- travel-dashboard/front-end/src/app/index.ts
+++
@@ -1,2 +0,0 @@
-export {environment} from './environment';
-export {TravelDashboardAppComponent} from './travel-dashboard.component';
